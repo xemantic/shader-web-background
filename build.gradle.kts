7d8f8686882e2ffd97b867749df2e28fd44a8cf9
Kazik Pogoda <morisil@xemantic.com>
2020-10-20 17:17:02 +0200

    initial project files

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+  java
+}
+
+dependencies {
+  runtimeOnly("com.google.javascript:closure-compiler:v20201006")
+}
+
+repositories {
+  jcenter()
+}
+
+task("execute", JavaExec::class) {
+  group = "minify"
+  main = "com.google.javascript.jscomp.CommandLineRunner"
+  args = listOf(
+      "--compilation_level", "ADVANCED",
+      "--js", "shader-web-background.js",
+      "--js_output_file", "shader-web-background.min.js"
+  )
+  classpath = sourceSets["main"].runtimeClasspath
+}
