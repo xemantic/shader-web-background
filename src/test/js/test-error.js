6005ea16b58894793d61be31e6f74a35504186b7
Kazik Pogoda <morisil@xemantic.com>
2020-11-23 10:50:30 +0100

    test: small testing framework - copyright update

--- src/test/js/test-error.js
+++ src/test/js/test-error.js
@@ -1,7 +1,9 @@
 /*
+ * Copyright 2020  Kazimierz Pogoda
+ *
  * This file is part of shader-web-background.
  *
- *  shader-web-background is free software: you can redistribute it and/or modify
+ * shader-web-background is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
@@ -12,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ * along with shader-web-background.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 "use strict";
