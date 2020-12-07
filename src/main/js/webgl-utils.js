b7330a3023dc6543cf48b79c7d6f730082b57605
Kazik Pogoda <morisil@xemantic.com>
2020-12-07 14:52:24 +0100

    default texture wrap is REPEAT now

--- src/main/js/webgl-utils.js
+++ src/main/js/webgl-utils.js
@@ -390,8 +390,8 @@ class DoubleBuffer {
     this.strategy.setUpTexture(width, height);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
     gl.bindTexture(gl.TEXTURE_2D, null);
     return texture;
   }
