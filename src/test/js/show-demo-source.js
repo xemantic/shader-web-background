298cd1a62c5bcf44ee2a465575ccac41748b8185
Kazik Pogoda <morisil@xemantic.com>
2020-12-12 15:14:17 +0100

    show-demo-source.js, missing copyright statement

--- src/test/js/show-demo-source.js
+++ src/test/js/show-demo-source.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2020  Kazimierz Pogoda
+ *
+ * This file is part of shader-web-background.
+ *
+ * shader-web-background is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * shader-web-background is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with shader-web-background.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 "use strict";
 
 window.addEventListener("DOMContentLoaded", _ => {
